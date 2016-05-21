bf9d3801af9b7e5df6796d85a2c9508ae8e8a60a
Thomas Norén <thomas.noren@ciber.com>
2016-05-21 10:20:26 +0200

    initial

--- src/app/props-no.component.ts
+++ src/app/props-no.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { DashboardComponent } from './+dashboard';
+import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
+import { PersonService } from './shared'
 
 @Component({
   moduleId: module.id,
   selector: 'props-no-app',
   templateUrl: 'props-no.component.html',
-  styleUrls: ['props-no.component.css']
+  styleUrls: ['props-no.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS, PersonService]
 })
+@Routes([
+  {path: '/dashboard', component: DashboardComponent}
+])
 export class PropsNoAppComponent {
   title = 'props-no works!';
 }
